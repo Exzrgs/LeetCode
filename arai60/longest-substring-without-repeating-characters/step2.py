e7c4c3ca118bb4131089c8b9329fe7bdb124dab2
Exzrgs <s.nakao0120@gmail.com>
2024-04-27 11:07:57 +0900

    change seen to char_to_index

--- arai60/longest-substring-without-repeating-characters/step2.py
+++ arai60/longest-substring-without-repeating-characters/step2.py
@@ -3,17 +3,19 @@
 start_index → left
 enumerateを使う
 answerに代入する処理の位置を変える
+
+char_to_indexのほうがよさそう
 """
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = dict()
+        char_to_index = dict()
         answer = 0
         left = 0
         for right, c in enumerate(s):
-            if c in seen and seen[c] >= left:
-                left = seen[c] + 1
-            seen[c] = right
+            if c in char_to_index and char_to_index[c] >= left:
+                left = char_to_index[c] + 1
+            char_to_index[c] = right
             answer = max(answer, right - left + 1)
         
         return answer
