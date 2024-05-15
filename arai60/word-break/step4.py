c86425d624fd55a3eb31a12e0a859902fc59da68
Exzrgs <s.nakao0120@gmail.com>
2024-05-15 19:12:20 +0900

    add startswith

--- arai60/word-break/step4.py
+++ arai60/word-break/step4.py
@@ -37,3 +37,16 @@ class Solution:
             return False
         
         return can_word_break(0)
+
+# startswith
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        can_break = [False] * (len(s) + 1)
+        can_break[0] = True
+        for start in range(len(s) + 1):
+            if not can_break[start]:
+                continue
+            for word in wordDict:
+                if s.startswith(word, start):
+                    can_break[start + len(word)] = True
+        return can_break[len(s)]
