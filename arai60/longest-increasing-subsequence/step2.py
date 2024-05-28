cef615d6080e5a8833683bed37ffa71aa8984fbf
Exzrgs <s.nakao0120@gmail.com>
2024-05-29 00:03:31 +0900

    complete

--- arai60/longest-increasing-subsequence/step2.py
+++ arai60/longest-increasing-subsequence/step2.py
@@ -1,13 +1,24 @@
+"""
+ある時点での最大の長さを保持するほうが自然そう
+dpはいろいろなパターンがあるからどれが良いか比較検討できるとよい
+"""
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        max_lengths = [1] * len(nums)
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    max_lengths[i] = max(max_lengths[i], max_lengths[j] + 1)
+        return max(max_lengths)
+
+# 二分探索でより小さい値に更新する
+from bisect import bisect_left
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         MAX_NUM = 10 ** 4
-        MIN_NUM = -(10 ** 4)
-        min_for_lengths = [MAX_NUM + 1] * (len(nums) + 1)
-        min_for_lengths[0] = MIN_NUM - 1
-        for i in range(len(nums)):
-            for j in range(1, len(nums) + 1):
-                if nums[i] > min_for_lengths[j - 1]:
-                    min_for_lengths[j] = min(min_for_lengths[j], nums[i])
-        for i in reversed(range(1, len(nums) + 1)):
-            if min_for_lengths[i] != MAX_NUM + 1:
-                return i
+        increasing_subsequence = [MAX_NUM + 1] * len(nums)
+        for n in nums:
+            index = bisect_left(increasing_subsequence, n)
+            increasing_subsequence[index] = n
+        return bisect_left(increasing_subsequence, MAX_NUM + 1)
