6cdb880a99e5ed264e0c4941facb4f40e823f4e5
Exzrgs <s.nakao0120@gmail.com>
2024-06-19 17:07:21 +0900

    read document

--- arai60/maximum-subarray/step2.py
+++ arai60/maximum-subarray/step2.py
@@ -1,6 +1,44 @@
 """
 https://github.com/YukiMichishita/LeetCode/pull/13/files
     mathのinfを使っている。Pythonのinf周りは調べたことがなかったので、これを機に詳しく調べてみよう
+    
+    https://docs.python.org/ja/3/library/math.html#math.inf
+    math.inf
+        float('inf')と等価
+        math.isinf()で確認できる
+        
+        float
+            https://docs.python.org/ja/3/library/functions.html#float
+            floatはクラスで、float()でコンストラクタを呼び出している。この辺よく考えたことなかったけど、たしかにそうだ
+            だからfloatクラスに文字列型を引数としてとるコンストラクタを作っておく。それでfloat("inf")とかを表現する
+            
+            <- 一般の Python オブジェクト x に対して、float(x) は x.__float__() に委譲します。 __float__() が定義されていない場合、__index__() へフォールバックします。
+                https://docs.python.org/ja/3/reference/datamodel.html#object.__float__
+                __float__()は数値型のオブジェクトに実装されているメソッド。数値型の抽象クラスに抽象メソッドとして定義されている?
+                このdatamodelのドキュメント、今じゃなくていいが、javaの勉強が終わったタイミングで通読しよう
+                ~のドキュメント通読とかのPR作るとよい
+            <- 引数が与えられなければ、0.0 が返されます。
+        
+    math.nan
+        float('nan')と等価
+        math.isnan()で確認できる
+        
+        NAN
+            https://ja.wikipedia.org/wiki/NaN
+            非数
+            主に浮動小数点演算の結果として、不正なオペランドを与えられたために生じた結果を表す値またはシンボル
+            
+            quiet nan(qNAN)
+                不正な操作や不正な値で生じる誤りを伝播させるために使用
+            signaling nan(sNAN)
+                (読んだけどよくわからなかった)
+    math.e
+        自然対数の底
+    math.pi
+        π
+    
+    mathモジュール
+        ほとんどがC言語の数学ライブラリ関数に対する薄いラッパでできている
 
 https://github.com/SuperHotDogCat/coding-interview/pull/14/files
     cumulative sum: 累積和
