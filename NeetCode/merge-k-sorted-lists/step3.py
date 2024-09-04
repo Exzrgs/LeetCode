d925e9f17f245d3b9b7e97efccc127b013e35e47
Exzrgs <s.nakao0120@gmail.com>
2024-09-04 21:26:57 +0900

    complete

---
+++ NeetCode/merge-k-sorted-lists/step3.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        def merge_linked_list(l1, l2):
+            dummy = ListNode()
+            tail = dummy
+            while l1 and l2:
+                if l1.val < l2.val:
+                    tail.next = l1
+                    l1 = l1.next
+                else:
+                    tail.next = l2
+                    l2 = l2.next
+                tail = tail.next
+            if l1:
+                tail.next = l1
+            if l2:
+                tail.next = l2
+            return dummy.next
+        
+        if len(lists) == 0:
+            return None
+        merged = lists[0]
+        for i in range(1, len(lists)):
+            merged = merge_linked_list(merged, lists[i])
+        return merged
