fbab184db493f2f9e68f5b8a0c358e0bfbfb3150
Exzrgs <s.nakao0120@gmail.com>
2024-09-08 22:54:03 +0900

    complete

---
+++ NeetCode/lowest-common-ancestor-of-a-binary-search-tree/step1.py
@@ -0,0 +1,28 @@
+"""
+Question:
+    Is the value of node unique?
+    How is the num of tree nodes?
+Discussion:
+    We wanna go from root to under
+    When the routes to search p and q are different, the node is LCA
+Test:
+    
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        cur = root
+        while True:
+            if cur.val > p.val and cur.val > q.val:
+                cur = cur.left
+            elif cur.val < p.val and cur.val < q.val:
+                cur = cur.right
+            else:
+                return cur
