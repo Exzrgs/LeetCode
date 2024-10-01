906f7a493ca7eba9e5ffe15d12f23662875c9984
Exzrgs <s.nakao0120@gmail.com>
2024-10-02 00:49:36 +0900

    complete

---
+++ NeetCode/letter-combinations-of-a-phone-number/step1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        digits_to_letters = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        res = []
+        combs = []
+        
+        def dfs(index):
+            if index == len(digits):
+                res.append("".join(combs))
+                return
+            
+            for c in digits_to_letters[digits[index]]:
+                combs.append(c)
+                dfs(index + 1)
+                combs.pop()
+        
+        dfs(0)
+        return res
